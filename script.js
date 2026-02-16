9e542641866e6ca961fb8be258945f3b6e59827a
Morris <underwater_forests@outlook.com>
2026-02-16 16:13:28 -0500

    add completed game

---
+++ script.js
@@ -0,0 +1,76 @@
+// M 
+
+let humanScore = 0;
+let computerScore = 0;
+
+const getComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"]
+    let choice = choices[Math.floor(Math.random() * 3)];
+    console.log("Computer choice: " + choice);
+    return choice;
+}
+
+const getHumanChoice = () => {
+    let choice = prompt("Enter a choice of 'rock', 'paper' or 'scissors'.")
+    console.log("You selected: " + choice);
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    // paper beats rock
+    if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore += 1;
+        console.log("You win! Paper beats Rock.")
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore += 1;
+        console.log("You lose! Paper beats Rock.")
+    } else if (humanChoice === "rock" && computerChoice === "scissors") { // rock beats scissors 
+        humanScore += 1;
+        console.log("You win! Rock beats Scissors.")
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore += 1; 
+        console.log("You lose! Rock beats Scissors.")
+    } else if (humanChoice === "scissors" && computerChoice === "paper") { // scissors beats paper
+        humanScore += 1;
+        console.log("You win! Scissors beats Paper.")
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore += 1;
+        console.log("You lose. Scissors beats Paper.")
+    } else if (humanChoice === "rock" && computerChoice === "rock") { // else tie and go again
+        console.log("Tie! Go again!")
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+        console.log("Tie! Go again!")
+    } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+        console.log("Tie! Go again!")
+    }
+}
+
+function playGame() {
+    let totalRounds = 0;
+
+    // If total rounds are less than 5 end game and decide winner
+    while(totalRounds < 5) {
+        
+        // Need to declare winner to screen
+
+        // Always get a fresh choice from both players.
+        let humanChoice = getHumanChoice().toLowerCase();
+        let computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
+
+        totalRounds += 1;
+    }
+
+    console.log("STUB")
+
+    if (humanScore > computerScore) {
+        console.log("You are the winner!");
+    } else if (humanScore < computerScore) {
+        console.log("You lose. Better luck next time!");
+    } else {
+        console.log("Tie, time to play again for tiebreaker!");
+    }
+}
+
+playGame();
